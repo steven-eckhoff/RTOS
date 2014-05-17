089c3377d8046b8041acdb901be2b3d851daca24
Steven W. Eckhoff <steven_ece@utexas.edu>
2014-05-16 19:26:00 -0500

    Adding asm files for sychronization

---
+++ arch/arm/include/asm/atomic.h
@@ -0,0 +1,77 @@
+/*! \file atomic.h
+ *  \brief Low level atomic routines for ARMv7-M
+ */
+#ifndef ATOMIC_H
+#define ATOMIC_H
+
+#include "include/types.h"
+
+/*! \brief Increments the value of an atomic type 
+ */
+bool static inline atomic_inc(atomic_t *a)
+{
+	s32_t res;
+
+	__asm__ __volatile__ (
+"1:	ldrex %0, %1\n"
+"	add %0, #1\n"
+"	strex r0, %0, %1\n"
+"	cmp r0, #0\n"
+"	bne 1b\n"
+	: "+r" (res)
+	: "m" (a->value)
+	);
+
+	return (res >= 0 ? TRUE : FALSE);
+}
+
+/*! \brief Decrements the value of an atomic type
+ */
+bool static inline atomic_dec(atomic_t *a)
+{
+	s32_t res;
+
+	__asm__ __volatile__ (
+"1:	ldrex %0, %1\n"
+"	sub %0, #1\n"
+"	strex r0, %0, %1\n"
+"	cmp r0, #0\n"
+"	bne 1b\n"
+	: "+r" (res)
+	: "m" (a->value)
+	);
+
+	return (res < 0 ? TRUE : FALSE);
+}
+
+/*! \brief Writes value to atomic type
+ */
+void static inline atomic_write(atomic_t *a, s32_t value)
+{
+	__asm__ __volatile__ (
+"	mov %1, r1\n"
+"1:	ldrex r0, %0\n"
+"	strex r0, r1, %0\n"
+"	cmp r0, #0\n"
+"	bne 1b\n"
+	:
+	: "m" (a->value), "r" (value)
+	); 
+}
+
+/*! \brief Atomically adds one value to another
+ */
+void static inline atomic_add(atomic_t *a, s32_t value)
+{
+	__asm__ __volatile__ (
+"	ldrex r0, %0\n"
+"1:	add r0, r0, %1\n"
+"	strex r1, r0, %0\n"
+"	cmp r1, #0\n"
+"	bne 1b\n"
+	:
+	: "m" (a->value), "r" (value)
+	);
+}
+
+#endif // ATOMIC_H
---
+++ arch/arm/include/asm/lock.h
@@ -0,0 +1,27 @@
+/*! \file lock.h
+ *  \brief This file defines low leve lock routine for ARMv7-M
+ */
+#ifndef LOCK_H
+#define LOCK_H
+
+#include "include/lock.h"
+
+/*! \brief Spin lock routine for kernel lock object
+ */
+void spinlock(lock_t *l)
+{
+	atomic_t *a = &l->atomic;
+	
+	__asm__ __volatile__ (
+"	mov r0, #1\n"
+"1:	ldrex r1, %0\n"
+"	cmp r1, #0\n"
+"	itt eq\n"
+"	strexeq r1, r0, %0\n"
+"	cmpeq r1, #0\n"
+"	bne 1b"
+	:
+	: "m" (a->value) );
+}
+
+#endif // LOCK_H
