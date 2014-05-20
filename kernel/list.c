4e52e14285a84d12edf888ea824a92d031b7e900
Steven W. Eckhoff <steven_ece@utexas.edu>
2014-05-20 17:23:57 -0500

    Functions completed in list.c

--- kernel/list.c
+++ kernel/list.c
@@ -6,22 +6,69 @@
 
 #include "include/list.h"
 
-link_t* static inline list_remove_circular_double(list_t *list, link_t *member)
-{	
+link_t* static inline list_remove_linear_single(list_t *list, link_t *member)
+{
+	link_t *link_ptr;
+
 	if (NULL == list || NULL == member)
 		return NULL;
 
 	if (0 == list->member_count)
 		return NULL;
+	
+	if (list->member_count > 1) {
+		if (member == list->head) {
+			list->head = member->next;
+		} else {
+			if ((2 == list->member_count) 
+					&& (list->tail == member)) {
+				list->tail = list->head;
+				list->head->next = NULL;
+			} else { // Assumed less likely
+				link_ptr = list->head;
+				do {
+					link_ptr = link_ptr->next;
+				} while ((link_ptr != NULL) 
+						&& (link_ptr->next != member));
+
+				if (link_ptr == NULL) // Member is not in the list
+					return NULL;
+			
+				link_ptr->next = NULL;
+
+				if (member == list->tail)
+					list->tail = link_ptr;
+			}
+		}
+	}	
+	
+	member->next = NULL;
+	
+	--list->member_count;
+
+	return member;
+}
+
+link_t* static inline list_remove_linear_double(list_t *list, link_t *member)
+{	
+	if (NULL == list || NULL == member)
+		return NULL;
+	
+	if (0 == list->member_count)
+		return NULL;
 
 	if (list->member_count > 1) {
-		member->prev->next = member->next;
-		member->next->prev = member->prev;
 
-		if (member == list->head)
+		if (member == list->head) {
 			list->head = member->next;
-		else if (member == list->tail)
+			list->head->prev = NULL;
+		} else if (member == list->tail) {
 			list->tail = member->prev;
+			list->tail->next = NULL;
+		} else {
+			member->next->prev = member->prev;
+			member->prev->next = member->next;
+		}
 	} else {
 		list->head = NULL;
 		list->tail = NULL;
@@ -79,26 +126,22 @@ link_t* static inline list_remove_circular_single(list_t *list, link_t *member)
 	return member;
 }
 
-link_t* static inline list_remove_linear_double(list_t *list, link_t *member)
+link_t* static inline list_remove_circular_double(list_t *list, link_t *member)
 {	
 	if (NULL == list || NULL == member)
 		return NULL;
-	
+
 	if (0 == list->member_count)
 		return NULL;
 
 	if (list->member_count > 1) {
+		member->prev->next = member->next;
+		member->next->prev = member->prev;
 
-		if (member == list->head) {
+		if (member == list->head)
 			list->head = member->next;
-			list->head->prev = NULL;
-		} else if (member == list->tail) {
+		else if (member == list->tail)
 			list->tail = member->prev;
-			list->tail->next = NULL;
-		} else {
-			member->next->prev = member->prev;
-			member->prev->next = member->next;
-		}
 	} else {
 		list->head = NULL;
 		list->tail = NULL;
@@ -112,68 +155,148 @@ link_t* static inline list_remove_linear_double(list_t *list, link_t *member)
 	return member;
 }
 
-link_t* static inline list_remove_linear_single(list_t *list, link_t *member)
+s32_t static inline list_add_linear_single(list_t *list, link_t *member_next, link_t *member_new)
 {
 	link_t *link_ptr;
 
-	if (NULL == list || NULL == member)
-		return NULL;
-
-	if (0 == list->member_count)
-		return NULL;
-	
-	if (list->member_count > 1) {
-		if (member == list->head) {
-			list->head = member->next;
-		} else {
-			if ((2 == list->member_count) 
-					&& (list->tail == member)) {
-				list->tail = list->head;
-				list->head->next = NULL;
-			} else { // Assumed less likely
-				link_ptr = list->head;
-				do {
-					link_ptr = link_ptr->next;
-				} while ((link_ptr != NULL) 
-						&& (link_ptr->next != member));
-
-				if (link_ptr == NULL) // Member is not in the list
-					return NULL;
-			
-				link_ptr->next = NULL;
-
-				if (member == list->tail)
-					list->tail = link_ptr;
-			}
-		}
-	}	
-	
-	member->next = NULL;
-	
-	--list->member_count;
-
-	return member;
-}
-
-s32_t static inline list_add_linear_single(list_t *list, link_t *member_next, link_t *member_new)
-{
-	if (NULL == list || NULL == member_next || NULL == member_new)
+	if (NULL == list || NULL == member_new)
 		return -1;
 	
-	if (0 list->member_count) {
+	if (0 == list->member_count) { // Empty list
+		member_new->next = NULL;
 		list->head = member_new;
 		list->tail = member_new;
-		member_new->next = NULL;
-	} else if (member_next == list->head) {
+	} else if (member_next == list->head) { // Insert as head
 		member_new->next = list->head;
 		list->head = member_new;
-	} else if (NULL = member_next) {
+	} else if (NULL = member_next) { // Insert as tail
+		member->next = NULL;
 		list->tail->next = member_new;
 		list->tail = member_new;
-		member->next = NULL;
 	} else {
-		
+		link_ptr = list->head;	
+
+		while ((NULL != link_ptr) && (link_ptr->next != member_next))
+			link_ptr = link_ptr->next;
+
+		if (NULL == link_ptr) // member_next not in list
+			return NULL;
+
+		member_new->next = member_next;
+
+		link_ptr->next = member_new;
 	}
+	
+	++list->member_count;
+
+	return 0;
+}
+
+s32_t static inline list_add_linear_double(list_t *list, link_t *member_next, link_t *member_new)
+{
+	link_t *link_ptr;
+
+	if (NULL == list || NULL == member_new)
+		return -1;
+	
+	if (0 == list->member_count) { // Empty list
+		member_new->next = NULL;
+		member_new->prev = NULL;
+		list->head = member_new;
+		list->tail = member_new;
+	} else if (member_next == list->head) { // Insert as head
+		member_new->next = list->head;
+		member_new->prev = NULL;
+		list->head->prev = member_new;
+		list->head = member_new;
+	} else if (NULL = member_next) { // Insert as tail
+		member->next = NULL;
+		member->prev = list->tail;
+		list->tail->next = member_new;
+		list->tail = member_new;
+	} else {
+		member_new->next = member_next;
+		member_new->prev = member_next->prev;
+		member_next->prev->next = member_new;
+		member_next->prev = member_new;
+	}
+	
+	++list->member_count;
+
+	return 0;
+}
+
+s32_t static inline list_add_circular_single(list_t *list, link_t *member_next, link_t *member_new)
+{
+	link_t *link_ptr;
+
+	if (NULL == list || NULL == member_new)
+		return -1;
+	
+	if (0 == list->member_count) { // Empty list
+		member_new->next = member_new;
+		list->head = member_new;
+		list->tail = member_new;
+	} else if (member_next == list->head) { // Insert as head
+		member_new->next = list->head;
+		list->tail->next = member_new;
+		list->head = member_new;
+	} else if (NULL = member_next) { // Insert as tail
+		member->next = NULL;
+		list->tail->next = member_new;
+		list->tail = member_new;
+	} else {
+		link_ptr = list->head;	
+
+		while ((NULL != link_ptr) && (link_ptr->next != member_next))
+			link_ptr = link_ptr->next;
+
+		if (NULL == link_ptr) // member_next not in list
+			return NULL;
+
+		member_new->next = member_next;
+
+		link_ptr->next = member_new;
+	}
+	
+	++list->member_count;
+
+	return 0;
+}
+s32_t static inline list_add_circular_double(list_t *list, link_t *member_next, link_t *member_new)
+{
+	link_t *link_ptr;
+
+	if (NULL == list || NULL == member_new)
+		return -1;
+	
+	if (0 == list->member_count) { // Empty list
+		member_new->next = member_new;
+		member_new->prev = member_new;
+		list->head = member_new;
+		list->tail = member_new;
+	} else if (member_next == list->head) { // Insert as head
+		member_new->next = list->head;
+		member_new->prev = list->tail;
+		list->tail->next = member_new;
+		list->head->prev = member_new;
+		list->head = member_new;
+	} else if (NULL = member_next) { // Insert as tail
+		member->next = list->head;
+		member->prev = list->tail;
+		list->tail->next = member_new;
+		list->head->prev = member_new;
+		list->tail = member_new;
+	} else {
+		member_new->next = member_next;
+		member_new->prev = member_next->prev;
+		member_next->prev->next = member_new;
+		member_next->prev = member_new;
+	}
+	
+	++list->member_count;
+
+	return 0;
 }
 
 /*! \brief
@@ -231,7 +354,25 @@ link_t *list_remove(list_t *list, link_t *member)
 
 /*! \brief
  */
-void list_add(list_t *list, link_t *link_new)
+void list_add(list_t *list, link_t *member_next, link_t *member_new)
 {
+	u32_t type = (((u32_t)list->circular) << 1) + (u32_t)(list->double_link);
+
+	switch (type) {
+	case 0:
+		return list_add_linear_single(list, member_next, member_new);
+		break;
+	case 1:
+		return list_add_linear_double(list, member_next, member_new);
+		break;
+	case 2:
+		return list_add_circular_single(list, member_next, member_new);
+		break;
+	case 3:
+		return list_add_circular_double(list, member_next, member_new);
+		break;
+	}
+	// Should not reach here
+	return NULL;
 }
 
